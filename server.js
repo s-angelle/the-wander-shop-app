c5f03f4e1f2e156d105b8984b03bd0115aec747c
Sabrina A <sabrina@Sabrinas-MBP.net>
2022-07-27 19:04:12 -0600

    updated server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 const Adventure = require("./models/Adventure");
 
 // === CONNECTION TO DATABASE ===
-mongoose.connect(process.env.MONGO_URL, {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
